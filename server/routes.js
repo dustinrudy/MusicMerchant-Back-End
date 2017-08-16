90e3b0f3bf9ec2c2e4b5114ed085260c0bd4fabe
dustinrudy <drudyprop@gmail.com>
2017-08-16 14:57:43 -0700

    Front end is able to connect to server

--- server/routes.js
+++ server/routes.js
@@ -10,13 +10,18 @@ import path from 'path';
 module.exports = function(app) {
 
   // Insert routes below
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-  app.use('/api/users', require('./api/user'));
+  app.use('/users', require('./api/user'));
   app.use('/auth', require('./auth'));
 
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/listings', require('./api/listing'));
-  app.use('/api/zips', require('./api/zip'));
+  app.use('/things', require('./api/thing'));
+  app.use('/listings', require('./api/listing'));
+  app.use('/zips', require('./api/zip'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth)/*')
    .get(errors[404]);
